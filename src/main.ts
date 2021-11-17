0389f3f9544a0226e69632a614f1c8504a264a2d
Eduardo San Martin Morote <posva13@gmail.com>
2021-11-17 12:00:22 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { createPinia, PiniaVuePlugin } from 'pinia'
-import { PiniaDebounce } from '@pinia/plugin-debounce'
-import { debounce } from 'ts-debounce'
 
 const pinia = createPinia()
-pinia.use(PiniaDebounce(debounce))
 
 Vue.use(PiniaVuePlugin)
 new Vue({
