2f0cd7fd73b3d5ced4f3bca1be9b3f6582ba5cc2
Eduardo San Martin Morote <posva13@gmail.com>
2021-12-03 11:46:09 +0100

    add setup store to tet

---
+++ src/stores/setupStore.ts
@@ -0,0 +1,12 @@
+import { ref } from '@vue/composition-api'
+import { defineStore } from 'pinia'
+
+export const useSetupStore = defineStore('setup-store', () => {
+  const counter = ref(0)
+
+  function doubleTheCounter() {
+    counter.value *= 2
+  }
+
+  return { counter, doubleTheCounter }
+})
